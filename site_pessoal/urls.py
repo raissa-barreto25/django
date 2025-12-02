3ca5faef4c17cdb37e1507be6cc7b1845787a7e1
raissa-barreto25 <rrbsiilva@gmail.com>
2025-12-02 15:20:57 +0000

    Django_blog

--- site_pessoal/urls.py
+++ site_pessoal/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for blog project.
+URL configuration for site_pessoal project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    #path('', include('blog.urls')),
 ]
